82cabc02d7eb7cd4cfd482158a2b7d443a25a8db
Jellyae <younesayman0@gmail.com>
2025-07-04 22:44:42 +0100

    Fix Customers table column names and sizes

--- task_2.sql
+++ task_2.sql
@@ -23,8 +23,8 @@ CREATE TABLE Books (
 
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
     phone VARCHAR(20),
     address TEXT
 );
