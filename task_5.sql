2dc008459f8aba5e3e3dfda88b4281da076edd33
Jellyae <younesayman0@gmail.com>
2025-07-04 23:14:21 +0100

    Add single row insertion for Customers table

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,6 @@
-REPLACE INTO customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.')
+ON DUPLICATE KEY UPDATE
+customer_name = VALUES(customer_name),
+email = VALUES(email),
+address = VALUES(address);
